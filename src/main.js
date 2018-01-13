d5549edb912c6daeff3aeba66bdb1e1a2b16ca3c
riskinputra <riski.n.putra@gmail.com>
2018-01-13 16:55:00 +0700

    config firebase

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueFire from 'vuefire'
+import * as firebase from 'firebase'
+import Vuetify from 'vuetify'
+import('vuetify/dist/vuetify.min.css')
 
+Vue.use(Vuetify, VueFire)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -11,5 +16,15 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyALoqi2ou3__v-fP_h-Js_zd217GW0VL8c',
+      uthDomain: 'kanban-a014c.firebaseapp.com',
+      databaseURL: 'https://kanban-a014c.firebaseio.com',
+      projectId: 'kanban-a014c',
+      storageBucket: 'kanban-a014c.appspot.com',
+      messagingSenderId: '720798796186'
+    })
+  }
 })
